4fcab4aa5ad47887b1bfda961cb090acdc81fa40
Detoeuf Alexandre <alexandre.detoeuf@epitech.eu>
2024-05-18 20:01:31 +0200

    [+] Add map with france forestfire and add api route to get all world wildfire

--- frontend/src/Components/NavigatorComponent.jsx
+++ frontend/src/Components/NavigatorComponent.jsx
@@ -4,17 +4,25 @@ import { useEffect } from "react";
 
 const NavigatorComponent = () => {
     useEffect(() => {
-        const map = new jsVectorMap({
-            selector: "#map",
-        });
-        map.setBackgroundColor("#FFFFFF")
-        return () => {
-            map.destroy();
-        };
+        fetch("/api/getData/FRA")
+            .then((res) => {
+                res.json()
+                    .then((data) => {
+                        const points = data.map(e => ({ coords: [e.latitude, e.longitude], style: { fill: 'steelblue' } }));
+
+                        const map = new jsVectorMap({
+                            selector: "#map",
+                            markers: points
+                        });
+                        return () => {
+                            map.destroy();
+                        };
+                    });
+            });
     }, [])
     return (
         <div className="h-400 w-400 relative">
-            <div id="map" style={{width: "100%", height: "100%"}}></div>
+            <div id="map" style={{width: "100%", height: "80vh"}} className='m-5'></div>
         </div>
     );
 }
--- frontend/src/Pages/NavPage.jsx
+++ frontend/src/Pages/NavPage.jsx
@@ -2,7 +2,6 @@ import Navbar from "../Components/Navbar.jsx";
 import Footer from "../Components/Footer.jsx";
 import NavigatorComponent from "../Components/NavigatorComponent.jsx";
 
-
 const NavPage = () => {
     return (
         <>
