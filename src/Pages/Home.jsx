18b2f92c6ad742e89db860c80c04446d1957367b
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-18 15:35:25 +0200

    [+] adding footer.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../Components/Navbar.jsx";
+import Footer from "../Components/Footer.jsx";
 
 
 export default function Home() {
     return (
         <>
             <Navbar />
+            <Footer />
         </>
-
     )
 }
